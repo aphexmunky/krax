41259353247448173311d3c32b3c4eb54e50d75f
Josh Hesketh <josh.hesketh@gmail.com>
2015-07-06 23:14:08 +0100

    set rest main actor to be spray example

--- application/rest/src/main/scala/Main.scala
+++ application/rest/src/main/scala/Main.scala
@@ -1,10 +1,54 @@
 import akka.actor.{ ActorSystem, Props }
 import com.typesafe.config.ConfigFactory
+import akka.io.IO
+import spray.can.Http
+import akka.pattern.ask
+import akka.util.Timeout
+import scala.concurrent.duration._
 
 object Main extends App {
 
 	val config = ConfigFactory.load
 
-	val system = ActorSystem("krax", config)
+	implicit val system = ActorSystem("krax", config)
+	implicit val timeout = Timeout(5 seconds)
 
+	val service = system.actorOf(Props[MyServiceActor], "demo-service")
+
+	IO(Http) ? Http.Bind(service, interface = "0.0.0.0", port = 8080)
+}
+
+import akka.actor.Actor
+import spray.routing._
+import spray.http._
+import MediaTypes._
+
+class MyServiceActor extends Actor with MyService {
+
+  // the HttpService trait defines only one abstract member, which
+  // connects the services environment to the enclosing actor or test
+  def actorRefFactory = context
+
+  // this actor only runs our route, but you could add
+  // other things here, like request stream processing
+  // or timeout handling
+  def receive = runRoute(myRoute)
+}
+
+trait MyService extends HttpService {
+
+  val myRoute =
+    path("") {
+      get {
+        respondWithMediaType(`text/html`) { // XML is marshalled to `text/xml` by default, so we simply override here
+          complete {
+            <html>
+              <body>
+                <h1>Say hello to <i>spray-routing</i> on <i>spray-can</i>!</h1>
+              </body>
+            </html>
+          }
+        }
+      }
+    }
 }
