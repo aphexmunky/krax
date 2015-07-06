6596791055c39d827e5c9280e0a1f8515c23db80
Josh Hesketh <josh.hesketh@gmail.com>
2015-07-06 12:42:49 +0100

    create a cluster singleton and schedule messages to it

--- application/backend/src/main/scala/Main.scala
+++ application/backend/src/main/scala/Main.scala
@@ -1,5 +1,10 @@
-import akka.actor.{ ActorSystem, Props }
+import akka.actor.{ ActorSystem, Props, PoisonPill }
 import com.typesafe.config.ConfigFactory
+
+import akka.contrib.pattern.{ ClusterSingletonManager, ClusterSingletonProxy }
+import scala.concurrent.duration._
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Main extends App {
 
@@ -7,4 +12,24 @@ object Main extends App {
 
 	val system = ActorSystem("krax", config)
 
+    system.actorOf(ClusterSingletonManager.props(
+      singletonProps = Props[Test], singletonName = "test",
+      terminationMessage = PoisonPill, role = Some("backend")),
+      name = "singleton")
+
+    val testActor = system.actorOf(ClusterSingletonProxy.props(singletonPath = "/user/singleton/test",
+    role = Some("backend")), name = "testProxy")
+
+    Thread.sleep(6000)
+    system.scheduler.schedule(6 seconds, 1 second)(testActor ! "testing 123")
+
+}
+
+import akka.actor.{ Actor, ActorLogging }
+
+class Test extends Actor with ActorLogging {
+	log.info("!!! ********* Test Actor Started ********* !!!")
+	def receive = {
+		case msg => log.info(s"********* test received: [$msg] *********")
+	}
 }
