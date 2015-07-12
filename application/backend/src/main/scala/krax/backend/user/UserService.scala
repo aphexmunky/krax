d4d93091b57044844dc19b7982da077a8172ca03
Josh Hesketh <josh.hesketh@gmail.com>
2015-07-12 07:29:49 +0100

    just fixing some spacing that was annoying

--- application/backend/src/main/scala/krax/backend/user/UserService.scala
+++ application/backend/src/main/scala/krax/backend/user/UserService.scala
@@ -9,21 +9,21 @@ import krax.domain.User._
 class UserService extends Actor with ActorLogging {
 
 	def receive = {
-		case registration: Register => user forward registration
-		case getEmail: GetEmail 	=> user forward getEmail
-		case add: AddEmail 			=> user forward add
+		case registration: Register         ⇒ user forward registration
+		case getEmail: GetEmail             ⇒ user forward getEmail
+		case add: AddEmail                  ⇒ user forward add
 	}
 
 	val idExtractor: ShardRegion.IdExtractor = {
-		case msg @ Register(username, _)	⇒ (username, msg)
-		case get @ GetEmail(username)		⇒ (username, get)
-		case add @ AddEmail(username, _, _)	⇒ (username, add)
+		case msg @ Register(username, _)    ⇒ (username, msg)
+		case get @ GetEmail(username)       ⇒ (username, get)
+		case add @ AddEmail(username, _, _) ⇒ (username, add)
 	}
 
 	val shardResolver: ShardRegion.ShardResolver = {
-		case Register(username, _)			⇒ (username.hashCode % 12).toString
-		case GetEmail(username)				⇒ (username.hashCode % 12).toString
-		case AddEmail(username, _, _)		⇒ (username.hashCode % 12).toString
+		case Register(username, _)          ⇒ (username.hashCode % 12).toString
+		case GetEmail(username)             ⇒ (username.hashCode % 12).toString
+		case AddEmail(username, _, _)       ⇒ (username.hashCode % 12).toString
 	}
 
 	val user: ActorRef = ClusterSharding(context.system).start(
